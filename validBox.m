7ac6bf75c306bb7e63687881175cba3b07d61256
Jorick Spitzen <jorick.spitzen@veneficus.nl>
2014-01-16 15:07:46 +0100

    Sanity check voor LP losser gemaakt

--- validBox.m
+++ validBox.m
@@ -1,5 +1,5 @@
 function valid = validBox(box)
 % Sanity checks to only return a box that is likely to be a real license
 % plate
-    valid = (box(3) > 80);
+    valid = (box(3) > 80) & (box(4) > 20);
 end
