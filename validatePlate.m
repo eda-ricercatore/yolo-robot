39948ac1875cebefd3f788fb3d8f20681582a0cb
Jorick Spitzen <jorick.spitzen@veneficus.nl>
2014-01-21 15:15:33 +0100

    Regexp improvements

--- validatePlate.m
+++ validatePlate.m
@@ -6,11 +6,17 @@ function valid = validatePlate(plateIn)
 % at http://nl.wikipedia.org/wiki/Nederlands_kenteken
     
     % First check for sidecodes 7 & 8
-    if (regexp(plateIn,'.+[A-Z]{3}.+'))
+    if (regexp(plateIn,'(^\d[A-Z]{3}\d{2}$)|(^\d{2}[A-Z]{3}\d$)'))
         valid = regexprep(plateIn,'[A-Z]{3}','-$0-');
         return
     end
     
-    % Otherwise, just insert after 2 and 4
+    % Otherwise, check for groups of 2
+    if (isempty(regexp(plateIn, '^([A-Z]{2}|\d{2}){3}$')))
+        valid = '';
+        return
+    end
+
+    % Finally, just insert after 2 and 4
     firstDash = regexprep(plateIn,'^.{2}','$0-');
-    valid     = regexprep(firstDash,'.{2}$','-$0');   
+    valid     = regexprep(firstDash,'.{2}$','-$0');
