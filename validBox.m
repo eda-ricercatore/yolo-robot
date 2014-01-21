6457df5e8dca0a18ba124d63ba7a4dc3ed31178f
Jorick Spitzen <jorick.spitzen@veneficus.nl>
2014-01-21 10:30:04 +0100

    Sanity check for border touch implemented

--- validBox.m
+++ validBox.m
@@ -1,5 +1,25 @@
 function valid = validBox(box)
 % Sanity checks to only return a box that is likely to be a real license
 % plate
-    valid = (box(3) > 80) & (box(4) > 20);
+    valid = true;
+    % The box must not touch a border, the probability of an incomplete
+    % plate is too high
+    if (box(1) == 0.5 || box(1)+box(3) == 640.5)
+        valid = false
+        return;
+    end
+    if (box(2) == 0.5 || box(2)+box(4) == 480.5)
+        valid = false
+        return;
+    end
+
+    % Minimum height and width
+    if (box(3) < 80)
+        valid = false;
+        return;
+    end
+    if (box(4) < 20)
+        valid = false;
+        return;
+    end
 end
