44958e3205f044430efb05bd605be2cf73b9d550
Jorick Spitzen <jorick.spitzen@veneficus.nl>
2014-01-21 12:09:22 +0100

    Use regular expressions to insert dashes into plate

---
+++ validatePlate.m
@@ -0,0 +1,16 @@
+function valid = validatePlate(plateIn)
+%VALIDATEPLATE Takes a string containing the license plate digits/letters
+%without dashed and returns a correct plate string
+
+% There are several layouts for a license plate, all described 
+% at http://nl.wikipedia.org/wiki/Nederlands_kenteken
+    
+    % First check for sidecodes 7 & 8
+    if (regexp(plateIn,'[A-Z]{3}'))
+        valid = regexprep(plateIn,'[A-Z]{3}','-$0-');
+        return
+    end
+    
+    % Otherwise, just insert after 2 and 4
+    firstDash = regexprep(plateIn,'^.{2}','$0-');
+    valid     = regexprep(firstDash,'.{2}$','-$0');   
