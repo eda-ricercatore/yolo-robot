1e283a9b01d580d36a658091fede77d0908bfee1
unknown <mephistophyles@gmail.com>
2014-01-22 11:22:57 +0100

    updated the bw threshold to if statements based on intensity

--- plateident.m
+++ plateident.m
@@ -9,11 +9,28 @@ load letterarray
 % it is apparently computationally efficient to convert the image to the
 % grayscale before converting it to a black and white image. 
 
-grplate = licenseplateimg(:,:,1);
-grplate1 = imcrop(grplate, [5 1 length(grplate(1,:,1))-10 length(grplate(:,1,1))]);
-bwplate = abs(im2bw(grplate1,0.5)-1);
-bwplate = imclearborder(bwplate);
+%%%%%%%%%%%%%% Make a variable intensity threshold based on the average
+%%%%%%%%%%%%%% intensity of a scene, somewhere between 0.4 and 0.6?
 
+sceneIntensityTemp = rgb2hsv(licenseplateimg);
+sceneIntensity = sceneIntensityTemp(:,:,3);
+iThresh = mean(max(sceneIntensity));
+if iThresh > 0.8
+    sceneIntensityAvg = 0.6;
+elseif iThresh > 0.6
+    sceneIntensityAvg = 0.5;
+else
+    sceneIntensityAvg = 0.4;
+end
+bwplate = abs(im2bw(sceneIntensity,sceneIntensityAvg)-1);
+% 
+% intensitythreshold = 0.4;
+% 
+% grplate = licenseplateimg(:,:,1);
+% grplate1 = imcrop(grplate, [5 1 length(grplate(1,:,1))-10 length(grplate(:,1,1))]);
+% bwplate = abs(im2bw(grplate1,intensitythreshold)-1);
+% bwplate = imclearborder(bwplate);
+% imshow(bwplate)
 %% perform some minor binary morphology operations on the image, 
 %  not necessary, we do this after we crop the image further
 
@@ -29,7 +46,6 @@ bwplate = imclearborder(bwplate);
 % bw2 = imclose(bw,se);
 objects = bwconncomp(bwplate,8);
 rp = regionprops(objects,'Area','Extrema');
-l = labelmatrix(objects);
 
 % subplot(2,2,1)
 % imshow(bwplate)
@@ -60,6 +76,8 @@ if (rp(sortedIndices(6)).Area < areaThres)
     outputString = '';
     return
 end
+newimage = bwplate;
+newimage(1,:) = zeros(1,length(bwplate(1,:)));
 for i = 1:6
     minx = min(minx,min(rp(sortedIndices(i)).Extrema(:,1)));
     miny = min(miny,min(rp(sortedIndices(i)).Extrema(:,2)));
@@ -67,9 +85,13 @@ for i = 1:6
     maxy = max(maxy,max(rp(sortedIndices(i)).Extrema(:,2)));
 end
 letterheight = maxy-miny;
-newimage = imcrop(bwplate,[minx miny maxx-minx maxy-miny]);
 
-clear minx miny maxx maxy i;
+newimage = imcrop(newimage,[minx-1 miny-1 maxx-minx+2 maxy-miny+2]);
+width = length(newimage(1,:));
+height = length(newimage(:,1));
+newimage = imclearborder(newimage);
+% imshow(newimage)
+clear minx miny maxx maxy i width;
 
 %% discretize the objects into distinct objects
 objects = bwconncomp(newimage,8);
@@ -98,7 +120,7 @@ for j = 1:length(rp2)
         %     tempstr = imcompare(letter,letterheight, maxx-minx); %replaced by
         %     scaling and iteration over array.
         compar = zeros(length(letterArray),1);
-        temp1 = imresize(letter, [60 40]);
+        templ = imresize(letter, [60 40]);
         for i = 1:length(letterArray)
             % find letterarray(i,1) height and width
             testletter = letterArray{i,1};
@@ -126,4 +148,4 @@ for j = 1:length(rp2)
     end
 end
 outputString = oString;
-clear minx miny maxx maxy letterheight letter tempstr;
+clear minx miny maxx maxy letterheight letter tempstr letterArray;
